590ebf42fda46a270f677f8ecb8fb851cef635d9
cactusaurelius <39528088+cactusaurelius@users.noreply.github.com>
2021-05-06 02:15:58 +0300

    create watcher

--- utilities/libs/Watchers/Watchers.ts
+++ utilities/libs/Watchers/Watchers.ts
@@ -22,4 +22,45 @@ const MEASUREMENT_OHLC = 'OHLC';
 export class Watchers {
 	public static runningWatchers: Watcher[] = [];
 	public static influx: Influx;
+
+  public static async createWatcher({
+    watcherConfig,
+  }: {
+    watcherConfig: IWatcherConfig;
+  }): Promise<{ id: string } | string> {
+    try {
+      if (watcherConfig.id) {
+        return 'Cannot create a new watcher with an id';
+      }
+
+      if (!(<any>watcherClasses)[watcherConfig.type]) {
+        return `Watcher type ${watcherConfig.type} doesn't exists`;
+      }
+
+      if (await watcherConfigurationExist(watcherConfig)) {
+        return `Watcher configuration already exist`;
+      }
+
+      const watcher: Watcher = new (<any>watcherClasses)[watcherConfig.type](
+        watcherConfig
+      );
+
+      try {
+        await watcher.init();
+      } catch (error) {
+        return error;
+      }
+      watcher.setInflux(this.influx);
+      watcher.run().catch(error => {
+        throw error;
+      });
+
+      Watchers.runningWatchers.push(watcher);
+      return { id: watcher.id };
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
+
 }
