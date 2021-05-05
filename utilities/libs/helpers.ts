1b03443bf7569a9784062af8ed3dd65d0431a8bb
cactusaurelius <39528088+cactusaurelius@users.noreply.github.com>
2021-05-06 02:26:21 +0300

    general helpers

---
+++ utilities/libs/helpers.ts
@@ -0,0 +1,51 @@
+import * as nodemailer from 'nodemailer';
+import {logger} from '../logger';
+const config = {
+	mail: {
+		service: 'gmail',
+		user: 'username@gmail.com',
+		pass: 'password',
+	},
+};
+/**
+ * Sleep helper
+ *
+ * @export
+ * @param {number} ms
+ * @returns {Promise<{}>}
+ */
+export function sleep(
+	ms: number,
+	intervalRef?: {id: NodeJS.Timeout | undefined}
+): Promise<{}> {
+	return new Promise(resolve => {
+		const id = setTimeout(resolve, ms);
+		if (intervalRef) {
+			intervalRef.id = id;
+		}
+	});
+}
+
+export function sendMail(msg: string) {
+	if (config.mail) {
+		const {service, user, pass} = config.mail;
+		const transporter = nodemailer.createTransport({
+			service,
+			auth: {
+				user,
+				pass,
+			},
+		});
+
+		const mailOptions = {
+			from: user,
+			to: user,
+			subject: 'influx-crypto-watcher',
+			text: msg,
+		};
+
+		transporter.sendMail(mailOptions, (error, info) => {
+			error ? logger.error(error) : logger.info('Email sent: ' + info.response);
+		});
+	}
+}
